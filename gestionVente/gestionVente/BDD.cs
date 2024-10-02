e6bfa27d863638fb299b8014f1a4bfded492629e
Léo Laheu <leo.laheu@gmail.com>
2024-10-02 11:58:47 +0200

    version1

---
+++ gestionVente/gestionVente/BDD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace GestionVente
+{
+    public class BDD
+    {
+        private MySqlConnection laConnexion;
+        
+        string maChaineConnexion = "";
+
+        public BDD(){
+
+        }
+    }
+}
