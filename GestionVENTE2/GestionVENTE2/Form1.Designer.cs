8201fa1ea1757f537fe747d97017389f731a7dfe
Léo Laheu <leo.laheu@gmail.com>
2024-10-02 12:35:15 +0200

    version1.0.0

---
+++ GestionVENTE2/GestionVENTE2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GestionVENTE2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ListeClient = new System.Windows.Forms.ListView();
+            this.Ajouter = new System.Windows.Forms.Button();
+            this.Supprimer = new System.Windows.Forms.Button();
+            this.modifier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(472, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // ListeClient
+            // 
+            this.ListeClient.Location = new System.Drawing.Point(31, 81);
+            this.ListeClient.Name = "ListeClient";
+            this.ListeClient.Size = new System.Drawing.Size(472, 310);
+            this.ListeClient.TabIndex = 1;
+            this.ListeClient.UseCompatibleStateImageBehavior = false;
+            this.ListeClient.SelectedIndexChanged += new System.EventHandler(this.ListeClient_SelectedIndexChanged);
+            // 
+            // Ajouter
+            // 
+            this.Ajouter.Location = new System.Drawing.Point(590, 150);
+            this.Ajouter.Name = "Ajouter";
+            this.Ajouter.Size = new System.Drawing.Size(75, 23);
+            this.Ajouter.TabIndex = 2;
+            this.Ajouter.Text = "Ajouter";
+            this.Ajouter.UseVisualStyleBackColor = true;
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.Location = new System.Drawing.Point(590, 218);
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Size = new System.Drawing.Size(75, 23);
+            this.Supprimer.TabIndex = 3;
+            this.Supprimer.Text = "Supprimer";
+            this.Supprimer.UseVisualStyleBackColor = true;
+            this.Supprimer.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // modifier
+            // 
+            this.modifier.Location = new System.Drawing.Point(590, 278);
+            this.modifier.Name = "modifier";
+            this.modifier.Size = new System.Drawing.Size(75, 23);
+            this.modifier.TabIndex = 4;
+            this.modifier.Text = "Modifier";
+            this.modifier.UseVisualStyleBackColor = true;
+            this.modifier.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.modifier);
+            this.Controls.Add(this.Supprimer);
+            this.Controls.Add(this.Ajouter);
+            this.Controls.Add(this.ListeClient);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ListView ListeClient;
+        private Button Ajouter;
+        private Button Supprimer;
+        private Button modifier;
+    }
+}
---
+++ GestionVENTE2/GestionVENTE2/Form1.cs
@@ -0,0 +1,25 @@
+namespace GestionVENTE2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListeClient_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
